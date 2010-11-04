6cbd3ad43614df1d22ec22e4f233c54d827af79b
liwei <hi@liwei.me>
2010-11-04 15:25:02 +0800

    rails3 basic files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ezuser::Application.config.secret_token = '8bbc5c0d70bc83a61c2e0e73edbacbfdd2b8f5bca9af5b33e5bccb310b73ad574ca134da70172283e8c70022ec0d34117ed8c7cf7b36b7135b1edb90ee69fb8c'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe User do
+  describe "user validate" do
+    it "create with email and password" do
+    end
+    it "login with email and password"
+    it "update name"
+  end
+end
